4c167437ae47ce4c19301406cd1b4ec26072b9c8
oui-ux <reinier.jan.s.castillo@gmail.com>
2026-04-10 02:15:52 +0800

    Add files via upload

---
+++ globalprotect-desktop.js
@@ -0,0 +1,79 @@
+// data/globalprotect-desktop.js
+// GlobalProtect Desktop taxonomy
+// Resolution intentionally ignored
+
+window.TAXONOMY_DATA["GlobalProtect Desktop"] = [
+
+  // ==================================================
+  // Login/Access | VPN app is stuck in Error Code 53003 state
+  // ==================================================
+  {
+    ci: "GlobalProtect Desktop",
+    category: "Application & Service",
+    subCategory: "Login/Access",
+    roleComponent: "VPN app is stuck in Error Code 53003 state",
+    closeCode: "Resolved Technical issue",
+    subCloseCode: "Deleted Tokens on Mac",
+    kb: ""
+  },
+  
+
+  {
+    ci: "GlobalProtect Desktop",
+    category: "Application & Service",
+    subCategory: "Login/Access",
+    roleComponent: "VPN app is stuck in Connecting",
+    closeCode: "Resolved Technical issue",
+    subCloseCode: "Reinstalled GPVPN app",
+    kb: ""
+  },
+
+  {
+    ci: "GlobalProtect Desktop",
+    category: "Application & Service",
+    subCategory: "Login/Access",
+    roleComponent: "System Extension Blocked",
+    closeCode: "Resolved Technical issue",
+    subCloseCode: "Changed the Security & Privacy settings",
+    kb: ""
+  },
+  // ==================================================
+  // Login/Access | Multiple accounts visible
+  // ==================================================
+  {
+    ci: "GlobalProtect Desktop",
+    category: "Application & Service",
+    subCategory: "Login/Access",
+    roleComponent: "VPN app, I see multiple accounts to login to",
+    closeCode: "Resolved Technical issue",
+    subCloseCode: "Signed in using other account to sync",
+    kb: ""
+  },
+
+  // ==================================================
+  // Login/Access | Connected - Internal
+  // ==================================================
+  {
+    ci: "GlobalProtect Desktop",
+    category: "Application & Service",
+    subCategory: "Login/Access",
+    roleComponent: "Connected - Internal",
+    closeCode: "Resolved Technical issue",
+    subCloseCode: "Connected to best available gateway",
+    kb: ""
+  },
+
+  // ==================================================
+  // Login/Access | Connection Failed
+  // ==================================================
+  {
+    ci: "GlobalProtect Desktop",
+    category: "Application & Service",
+    subCategory: "Login/Access",
+    roleComponent: "Connection Failed",
+    closeCode: "Resolved Technical issue",
+    subCloseCode: "Deleted the supported files",
+    kb: ""
+  }
+
+];
